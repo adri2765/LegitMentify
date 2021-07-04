afee4c47e66210a2a2c32f1e275b7ae3552c9829
adri2765 <72596789+adri2765@users.noreply.github.com>
2021-07-03 23:27:40 -0700

    base

---
+++ app/src/main/java/com/example/mentifyapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mentifyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openHome();
+            }
+        });
+    }
+    public void openHome(){
+        Intent intent=new Intent(this,homepage.class);
+        startActivity(intent);
+    }
+}
